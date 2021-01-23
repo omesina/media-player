bb3573bbdd23ca282bb8492f7db3cb9f81e85014
omesina <ormesina@outlook.com>
2021-01-22 23:55:40 -0600

    MediaPlayer ver 1.0.1, se agrego el modulo MediaPlayer.js

--- assets/index.js
+++ assets/index.js
@@ -1,15 +1,7 @@
+import MediaPlayer from "./MediaPlayer.js";
+
 const video = document.querySelector("video");
 const button = document.querySelector("button");
-
-function MediaPlayer(config) {
-    this.media = config.el;              
-}
-
-MediaPlayer.prototype.play = function () {            
-  (this.media.paused)
-  ? this.media.play()
-  : this.media.pause();              
-}
 
 const player = new MediaPlayer({el: video});
 
