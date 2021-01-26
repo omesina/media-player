97d2dcd584e0f9414fe0cefc04c9227cc1e861a5
omesina <ormesina@outlook.com>
2021-01-25 20:16:12 -0600

    Se actualizo función de Plugin: Autoplay.js

--- assets/plugins/AutoPlay.js
+++ assets/plugins/AutoPlay.js
@@ -4,16 +4,12 @@ function AutoPlay() {
 
 AutoPlay.prototype.run = function (player) {
     if(!player.muted)
-    {
         player.muted = true;
-    }
     
     player.play();
 
     if(player.muted)
-    {
         player.muted = false;
-    }
 }
 
 export default AutoPlay;
