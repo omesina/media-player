10a2f3a655953b533ee71a2fcd62949df1f0444d
omesina <ormesina@outlook.com>
2021-02-05 23:23:34 -0600

    Proyecto terminado

--- assets/index.ts
+++ assets/index.ts
@@ -1,13 +1,14 @@
-import MediaPlayer from "./MediaPlayer.ts";
-import AutoPlay from './plugins/AutoPlay.ts'
-import AutoPause from './plugins/AutoPause.ts'
+import MediaPlayer from "./MediaPlayer";
+import AutoPlay from './plugins/AutoPlay'
+import AutoPause from './plugins/AutoPause'
+import Ads from './plugins/Ads/'
 
 const video = document.querySelector("video");
 const playButton: HTMLElement = document.querySelector("#playButton");
 const mutedButton: HTMLElement = document.querySelector("#mutedButton");
 
 //const player = new MediaPlayer({el: video, plugins: []});
-const player = new MediaPlayer({el: video, plugins: [new AutoPlay(), new AutoPause()]});
+const player = new MediaPlayer({el: video, plugins: [new AutoPlay(), new AutoPause(), new Ads()]});
 
 playButton.onclick = () => player.tooglePlay();
 mutedButton.onclick = () => player.toogleMuted();
